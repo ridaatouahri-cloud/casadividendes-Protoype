0064b5fafdf5ffd4218c1164d6202f2005a174ca
ridaatouahri-cloud <rida.atouahri@gmail.com>
2025-10-22 16:49:56 +0000

    Updated Company.jsx

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,5 +1,4 @@
 import React, { useMemo, useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   LineChart, Line, XAxis, YAxis, Tooltip as RTooltip, ResponsiveContainer,
