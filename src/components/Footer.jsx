26637e2e3568cb2b067abccd6970f0a3d67bf5ca
ridaatouahri-cloud <rida.atouahri@gmail.com>
2025-10-24 13:56:08 +0000

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ const PassionIcon = ({ className = "h-6 w-auto" }) => (
   fontWeight="100"
   fill="url(#passionGradient)"
   textAnchor="middle"
-  transform="rotate(-25 100 50)"
+  transform="rotate(-15 100 50)"
   style={{ fontStyle: 'italic' }}
 >
       Passion
