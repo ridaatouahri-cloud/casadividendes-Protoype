b923717204cf8b0fd740fc60993e0f541a4782b2
ridaatouahri-cloud <rida.atouahri@gmail.com>
2025-10-24 13:56:20 +0000

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,7 +38,7 @@ const PassionIcon = ({ className = "h-6 w-auto" }) => (
     
     {/* Texte "Passion" stylisé en cursive */}
    <text 
-  x="100" 
+  x="120" 
   y="40" 
   fontFamily="cursive, 'Brush Script MT', 'Segoe Script', serif" 
   fontSize="48" 
